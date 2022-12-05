c821254090af1850c6a65532ebaab3ca4cb89173
Johanna Wirell <jw223gx@student.lnu.se>
2022-12-05 16:52:28 +0100

    Add images to top rated

--- app/src/components/home/browse/top_rated/TopRated.jsx
+++ app/src/components/home/browse/top_rated/TopRated.jsx
@@ -5,27 +5,33 @@
  * @version 1.0.0
  */
 
+import React, { lazy } from 'react'
+const Movie = lazy(() => import('../movie/Movie'))
+
 /**
  * Top rated movies component of application.
  * 
  * @return {HTML} - Render start page with public recipes.
  */
- export default function Popular () {
-  // Fetch popular
-  return (
-    <>
-    <h1 className="title">Top rated</h1>
-      <div className="top_rated">
-        <div className="top_rated_item"></div>
-        <div className="top_rated_item"></div>
-        <div className="top_rated_item"></div>
-        <div className="top_rated_item"></div>
-        <div className="top_rated_item"></div>
-        <div className="top_rated_item"></div>
-        <div className="top_rated_item"></div>
-        <div className="top_rated_item"></div>
-      </div>
-    </>
-  )
+ export default function Popular (props) {
+  if (props.movies) {
+
+    return (
+      <>
+      <h1 className="title">Top rated</h1>
+        <div className="top_rated">
+          {props.movies.map(movie => (
+            <Movie 
+              title={movie.original_title} 
+              posterPath={movie.poster_path} 
+              id={movie.id} 
+              className='top_rated_item'
+              key={movie.id}
+              />
+          ))}
+        </div>
+      </>
+    )
+  }
 }
   
