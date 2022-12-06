ca15f722b6c4aee64b7fb10e8a1aac1e082d5331
Johanna Wirell <jw223gx@student.lnu.se>
2022-12-06 17:19:41 +0100

    Arrows slide images

--- app/src/components/home/browse/popular/Popular.jsx
+++ app/src/components/home/browse/popular/Popular.jsx
@@ -5,6 +5,8 @@
  * @version 1.0.1
  */
 import React, { lazy } from 'react'
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
+
 const Movie = lazy(() => import('../movie/Movie'))
 
 /**
@@ -13,20 +15,35 @@ const Movie = lazy(() => import('../movie/Movie'))
  * @return {HTML} - Render start page with public recipes.
  */
 export default function Popular (props) {
+  const slideLeft = () => {
+    const slider = document.querySelector('#popular')
+    slider.scrollLeft = slider.scrollLeft - 500
+  }
+
+  const slideRight = () => {
+    const slider = document.querySelector('#popular')
+    slider.scrollLeft = slider.scrollLeft + 500
+  }
+
   if (props.movies) {
     return (
       <div className="trending-container">
         <h1 className="title">Trending</h1>
-        <div className="trending">
-          {props.movies.map(movie => (
-            <Movie 
-              title={movie.original_title} 
-              posterPath={movie.poster_path} 
-              id={movie.id} 
-              className='trending-item'
-              key={movie.id}
-              />
-          ))}
+
+        <div className="scroll-container">
+          <MdChevronLeft className="arrow" onClick={slideLeft} />
+          <div className="trending scroll" id="popular">
+            {props.movies.map(movie => (
+              <Movie 
+                title={movie.original_title} 
+                posterPath={movie.poster_path} 
+                id={movie.id} 
+                className='trending-item'
+                key={movie.id}
+                />
+            ))}
+          </div>
+          <MdChevronRight className="arrow" onClick={slideRight} />
         </div>
       </div>
     )
--- app/src/components/home/browse/top_rated/TopRated.jsx
+++ app/src/components/home/browse/top_rated/TopRated.jsx
@@ -6,6 +6,7 @@
  */
 
 import React, { lazy } from 'react'
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 const Movie = lazy(() => import('../movie/Movie'))
 
 /**
@@ -14,21 +15,36 @@ const Movie = lazy(() => import('../movie/Movie'))
  * @return {HTML} - Render start page with public recipes.
  */
  export default function Popular (props) {
+  const slideLeft = () => {
+    const slider = document.querySelector('#top')
+    slider.scrollLeft = slider.scrollLeft - 500
+  }
+
+  const slideRight = () => {
+    const slider = document.querySelector('#top')
+    slider.scrollLeft = slider.scrollLeft + 500
+  }
+
   if (props.movies) {
 
     return (
       <div className="top-rated-container">
       <h1 className="title">Top rated</h1>
-        <div className="top-rated">
-          {props.movies.map(movie => (
-            <Movie 
-              title={movie.original_title} 
-              posterPath={movie.poster_path} 
-              id={movie.id} 
-              className='top-rated-item'
-              key={movie.id}
-              />
-          ))}
+
+      <div className="scroll-container">
+        <MdChevronLeft className="arrow" onClick={slideLeft} />
+          <div className="top-rated scroll" id="top">
+            {props.movies.map(movie => (
+              <Movie 
+                title={movie.original_title} 
+                posterPath={movie.poster_path} 
+                id={movie.id} 
+                className='top-rated-item'
+                key={movie.id}
+                />
+            ))}
+          </div>
+          <MdChevronRight className="arrow" onClick={slideRight} />
         </div>
       </div>
     )
