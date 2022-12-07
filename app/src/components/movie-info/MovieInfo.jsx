f2c7ef7396e26ae360c3b75cf3e3c1febb56a532
Johanna Wirell <jw223gx@student.lnu.se>
2022-12-07 16:50:47 +0100

    Add some movie info

--- app/src/components/movie-info/MovieInfo.jsx
+++ app/src/components/movie-info/MovieInfo.jsx
@@ -10,6 +10,7 @@ import ClipLoader from 'react-spinners/ClipLoader'
 import { getMovieInfo } from '../../lib/CommunicationAPI'
 const Error500 = lazy(() => import('../error/Error500'))
 const Side = lazy(() => import('../home/side/Side'))
+const Trailer = lazy(() => import('./trailer/Trailer'))
 
 
 /**
@@ -19,6 +20,7 @@ const Side = lazy(() => import('../home/side/Side'))
  */
  export default function MovieInfo (props) {
   const [data, setData] = useState(null)
+  const [year, setYear] = useState(null)
   const [serverError, setServerError] = useState(null)
 
   useEffect(() => {
@@ -26,7 +28,7 @@ const Side = lazy(() => import('../home/side/Side'))
 
     const loadData = async () => {
       const path = window.location.pathname
-      const id = path.split("/").pop()
+      const id = path.split('/').pop()
       const response = await getMovieInfo(id)
 
       if (mounted) {
@@ -34,8 +36,8 @@ const Side = lazy(() => import('../home/side/Side'))
           setServerError(true)
           console.error(response)
         } else {
-          console.log(response.data)
           setData(response.data)
+          setYear(response.data.release_date.split('-').shift())
         }
       }
     }
@@ -45,6 +47,21 @@ const Side = lazy(() => import('../home/side/Side'))
     }
   }, [])
 
+  const toHoursAndMinutes = totalMinutes => {
+    let result = ''
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60
+    if (hours > 0) {
+      result += `${hours} h`
+    }
+    if (minutes > 0) {
+      result += `${minutes} min`
+    }
+
+    return result
+
+  }
+
   //  Loading icon if data is not fetched 
    if (!data) {
     // If server error render error compoent 500.
@@ -52,7 +69,7 @@ const Side = lazy(() => import('../home/side/Side'))
       return <Error500 /> 
     }
     return (
-      <div className="movie-info-container">
+      <div className="home-container">
         <div className="loading">
           <ClipLoader color="red" size={80}/>
         </div>
@@ -63,7 +80,15 @@ const Side = lazy(() => import('../home/side/Side'))
    return (
     <div className="home-container">
       <Side/>
+      <div className="movie-info-container">
+        <Trailer/>
+        <h1>{data.title}</h1>
+        <div className="labels">
+          <p>{year}</p>
+          <p>{toHoursAndMinutes(data.runtime)}</p>
+          <p>{data.tagline}</p>
+        </div>
+      </div>
     </div>
   )
  }
-  
---
+++ app/src/components/movie-info/trailer/Trailer.jsx
@@ -0,0 +1,19 @@
+/**
+ * Trailer component of the application.
+ *
+ * @author Johanna Wirell <wirelljohanna@gmail.com>
+ * @version 1.0.0
+ */
+
+ /**
+  * Trailer component of application.
+  * 
+  * @return {HTML} - Render start page with public recipes.
+  */
+  export default function Trailer (props) {
+    return (
+     <div className="trailer-container">
+     </div>
+   )
+  }
+   
