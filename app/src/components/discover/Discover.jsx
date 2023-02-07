a04c2e996cc03fef6c73ce1722f6a7e72d31a459
Johanna Wirell <jw223gx@student.lnu.se>
2023-02-07 14:20:06 +0100

    Get genres

--- app/src/components/discover/Discover.jsx
+++ app/src/components/discover/Discover.jsx
@@ -6,10 +6,12 @@
  */
 
 import React, { lazy, useState, useEffect } from 'react'
-import { getMovies } from '../../lib/CommunicationAPI'
+import { discover, getGenres } from '../../lib/Discover'
 
 const Side = lazy(() => import('../home/side/Side'))
 const Search = lazy(() => import('../search/Search'))
+const Movie = lazy(() => import('../home/browse/movie/Movie'))
+const Loading = lazy(() => import('../loading/Loading'))
 const Error500 = lazy(() => import('../error/Error500'))
 
 /**
@@ -18,11 +20,81 @@ const Error500 = lazy(() => import('../error/Error500'))
 * @return {HTML} - Render the discover page.
 */
 export default function Discover () {
+    const [loading, setLoading] = useState(true)
+    const [serverError, setServerError] = useState(null)
+    const [result, setResult] = useState(null)
+    const [genres, setGenres] = useState(null)
+    useEffect(() => {
+        let mounted = true
+      
+        const loadData = async () => {
+          const response = await discover()
+          setLoading(true)
+          if (mounted) {
+            if (response.error) {
+              setServerError(true)
+              console.error(response)
+            } else {
+              setLoading(false)
+              setResult(response.data.results)
+            }
+          }
+        }
+      
+        const loadGenres = async () => {
+          const response = await getGenres()
+          setLoading(true)
+          if (mounted) {
+            if (response.error) {
+              setServerError(true)
+              console.error(response)
+            } else {
+              setLoading(false)
+              setGenres(response.data.genres)
+            }
+          }
+        }
+      
+        loadData()
+        loadGenres()
+      
+        return () => {
+          mounted = false
+        }
+      }, [])
  
+  if (serverError) {
+    return <Error500 />
+  } else if (loading) {
+    return <Loading />
+  }
   return (
     <div className="home-container">
       <Search/>
       <Side/>
+      <div className="discover-container">
+        <h1 className="title">Discover</h1>
+        <div className="options-container">
+            {genres && (
+                genres.map(genre => (
+                    <div className="discover-option">{genre.name}</div>
+                ))
+            )}
+        </div>
+        <div className="movies-container">
+            {result && (
+                result.map(movie => (
+                    <Movie 
+                    title={movie.original_title} 
+                    posterPath={movie.poster_path}
+                    id={movie.id} 
+                    className='discover-item'
+                    key={movie.id}
+                    />
+                ))
+            )}
+        </div>
+      </div>
 
     </div>
   )
---
+++ app/src/lib/Discover.js
@@ -0,0 +1,29 @@
+/**
+ * Functions for handling API communication.
+ *
+ * @author Johanna Wirell <wirelljohanna@gmail.com>
+ * @version 1.0.0
+ */
+
+import axios from 'axios'
+
+const API_URL = import.meta.env.VITE_API_URL
+const API_KEY = import.meta.env.VITE_API_KEY
+
+export async function discover () {
+  return await axios.get(API_URL + '/discover/movie', {
+    headers: {
+      'Authorization': `Bearer ${API_KEY}`
+    }
+  }).then(res => res)
+    .catch(err => err)
+}
+
+export async function getGenres () {
+  return await axios.get(API_URL + '/genre/movie/list', {
+    headers: {
+      'Authorization': `Bearer ${API_KEY}`
+    }
+  }).then(res => res)
+    .catch(err => err)
+}
